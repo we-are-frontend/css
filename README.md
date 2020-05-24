2afe5f705c13abe04a532eb879227ebd3b0e3cf1
unknown <nasreddine.skandrani@gmail.com>
2020-05-24 04:11:39 -0400

    now working

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # waf-css
 
-... to do
+```
+> yarn install
+> yarn start
+```
+
+access your example in the browser: http://localhost:8080/layout_XXX/try_XX/
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,18 @@
 var path = require('path');
  var webpack = require('webpack');
  module.exports = {
-     entry: {
-         vendor: '@babel/polyfill', 
-         'super-toastr': './super-toastr.js'
-     },
-     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        libraryTarget: 'umd',
-        library: 'SuperToastrLib'
-     },
+    entry: {
+        'index': './index.js'
+    },
+    output: {
+       path: path.resolve(__dirname, 'dist'),
+       filename: '[name].bundle.js',
+       libraryTarget: 'umd',
+       library: 'CssLib'
+    },
      module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]  //Preset used for env setup
-                    }
-                }
-            }
-         ]
+        ]
      },
      devServer: {
         writeToDisk: true,
